b8d7f7e69b3518b2bfb5b7be9f2f7163655572b4
tccsg <tccsg@aingbing.com>
2023-04-28 20:32:51 +0800

    chore

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -11,24 +11,4 @@ export * from '@mantine/hooks'
 export * from '@mantine/modals'
 export * from '@mantine/prism'
 export { Notifications, showNotification } from '@mantine/notifications'
-
 export * from '@mantine/core'
-
-// // Layout
-// export { Center, Container, Flex, Grid, Group, MediaQuery, SimpleGrid, Space, Stack } from '@mantine/core'
-// // Buttons
-// export { ActionIcon, Button, CloseButton, CopyButton, FileButton, UnstyledButton } from '@mantine/core'
-// // Inputs
-// export { Checkbox, ColorInput, ColorPicker, FileInput, NumberInput, PasswordInput, PinInput, Radio, Rating, Select, Slider, Switch, Textarea, TextInput } from '@mantine/core'
-// // Navigation
-// export { Anchor, Breadcrumbs, Burger, NavLink, Pagination, Tabs } from '@mantine/core'
-// // Data display
-// export { Accordion, Avatar, Badge, Card, ColorSwatch, Image, Indicator, ThemeIcon, Timeline } from '@mantine/core'
-// // Overlays
-// export { Dialog, Drawer, HoverCard, LoadingOverlay, Menu, Modal, Overlay, Popover, Tooltip } from '@mantine/core'
-// // Typography
-// export { Blockquote, Code, Highlight, List, Mark, Table, Text, Title, TypographyStylesProvider } from '@mantine/core'
-// // Feedback
-// export { Alert, Loader, Notification, Progress, Skeleton } from '@mantine/core'
-// // Miscellaneous
-// export { Box, Collapse, Divider, FocusTrap, Paper, Portal, ScrollArea, Transition } from '@mantine/core'
